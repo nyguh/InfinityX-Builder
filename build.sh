5b6466f5ca826f420c0585bf0f2e34beb5a2cb46
nyguh <1499296+nyguh@users.noreply.github.com>
2026-05-23 23:11:20 +0300

    Update Git user configuration and target phone

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ mkdir -p ~/android
 cd ~/android
 
 # Configure Git identities
-git config --global user.name "blueboysin"
-git config --global user.email "blueboysin@github.com"
+git config --global user.name "nyguh"
+git config --global user.email "afkbobobg@gmail.com"
 
 # Initialize Project Infinity-X 3.10 with depth limits
 repo init -u https://github.com/projectinfinity-x/manifest.git -b 16 --git-lfs --depth=1 --no-repo-verify
 
 # Copy your custom munch map into the local engine
 mkdir -p .repo/local_manifests
-cp /tmp/munch.xml .repo/local_manifests/munch.xml
+cp /tmp/macan.xml .repo/local_manifests/macan.xml
 
 # Super-slim down sync parameters (Extracts files without keeping git log histories)
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --current-branch
@@ -23,7 +23,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --current
 . build/envsetup.sh
 
 # Select POCO F4 as our target phone
-lunch infinity_munch-userdebug
+lunch infinity_macan-userdebug
 
 # Bake the ROM!
 mka bacon -j$(nproc --all)
